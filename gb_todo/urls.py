dc366d7dda2d85d51e6e06c3646d9c6eee4af94e
alexey <ae.polyakov@mail.ru>
2021-08-03 12:32:07 +0400

    token auth

--- gb_todo/urls.py
+++ gb_todo/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 from todoapp.views import ProjectViewSet, ToDoViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -29,4 +30,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
 ]
