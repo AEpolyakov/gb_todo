0801d34c79bcb1be2852e30cd1e2ff530e89ef0d
AEpolyakov <ae.polyakov@mail.ru>
2021-08-10 08:42:22 +0400

    test for APIRequestFactory, APIClient, APITestCase

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -17,15 +17,31 @@ class TestAPIRequestFactory(TestCase):
         self.projects = [mixer.blend(Project) for project in range(4)]
         self.admin = User.objects.create_superuser('admin@admin.ru', '1111')
 
-    def test_get_list(self):
-        # factory = APIRequestFactory()
+    def test_get_list_401(self):
+        request = self.factory.get('/api/users/')
+        view = ProjectViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_list_200(self):
         request = self.factory.get('/api/users/')
         force_authenticate(request, self.admin)
         view = ProjectViewSet.as_view({'get': 'list'})
         response = view(request)
-
-        print(f'testcase {response.data=}')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_todo(self):
+        request = self.factory.post('/api/todo/', {
+            'project': '1',
+            'text': 'test test test',
+            'created_by': "1",
+            'is_active': "true",
+            'is_deleted': "false",
+        })
+        force_authenticate(request, self.admin)
+        view = ToDoViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 class TestApiClient(TestCase):
@@ -33,14 +49,9 @@ class TestApiClient(TestCase):
     def setUp(self):
         self.users = [mixer.blend(User) for user in range(4)]
         self.projects = [mixer.blend(Project) for project in range(4)]
-
         self.admin_password = '1111'
         self.admin = User.objects.create_superuser('admin@admin.ru', self.admin_password)
         self.client = APIClient()
-
-    def test_get_user_list_401(self):
-        response = self.client.get(f'/api/users/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_user_list_200(self):
         self.client.login(username=self.admin.email, password=self.admin_password)
@@ -54,8 +65,19 @@ class TestApiClient(TestCase):
             'href': '',
             'users': ["1"],
         })
-        # print(f'{self.projects=}')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestApiTestCase(APITestCase):
+    def setUp(self):
+        self.users = [mixer.blend(User) for user in range(4)]
+        self.projects = [mixer.blend(Project) for project in range(4)]
+        self.admin_password = '1111'
+        self.admin = User.objects.create_superuser('admin@admin.ru', self.admin_password)
+
+    def test_get_user_list_401(self):
+        response = self.client.get(f'/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_patch_project(self):
         new_name = 'test_proj'
@@ -65,8 +87,3 @@ class TestApiClient(TestCase):
         })
         self.assertEqual(response.data["name"], new_name)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class TestApiTestCase(APITestCase):
-    pass
-
-
